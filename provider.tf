49f0ce963278e192c99a4e051b106e36b37c12a1
kshitejkhankal <kshitejkhankal@gmsil.com>
2024-09-12 10:11:17 +0530

    terraform code for ec2

--- provider.tf
+++ provider.tf
@@ -9,6 +9,6 @@ terraform {
 
 provider "aws" {
    region = "ap-south-1"
-#    access_key = "AKIA6ODU4ZLCGDT2PGG3"
-#    secret_key = "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"
+   access_key = "AKIA6ODU4ZLCGDT2PGG3"
+   secret_key = "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"
 }
